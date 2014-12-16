834e5972dc23cb0334b5e1670c6e595b9c941a3e
jimmycao <jimmy.cao@emc.com>
2014-12-16 05:37:43 -0800

    add sh

--- src/loop-with-ckpt-callback.c
+++ src/loop-with-ckpt-callback.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int main() {
 	int i;
 	for (i = 0; i < 1000; i++) {
-		fprintf(stderr, "%d\n", i);	
+		printf("%d\n", i);	
 		sleep(1);
 	}
 	return 0;
